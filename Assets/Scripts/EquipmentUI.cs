62bb49e41d05911761cfe41b03871952e582a74a
Antonio <roccanator@gmail.com>
2018-03-09 18:30:41 -0500

    Equipment is now a part of the new menu

--- Assets/Scripts/EquipmentUI.cs
+++ Assets/Scripts/EquipmentUI.cs
@@ -4,29 +4,16 @@ using TMPro;
 public class EquipmentUI : MonoBehaviour {
 
     public Transform itemsParent;
-    public GameObject equipmentUI;
-    public TextMeshProUGUI tooltip;
 
     EquipmentManager equipmentManager;
     EquipmentSlot[] slots;
 
-    // Use this for initialization
     void Start()
     {
         equipmentManager = EquipmentManager.instance;
         equipmentManager.onEquipmentChanged += UpdateUI;
 
         slots = itemsParent.GetComponentsInChildren<EquipmentSlot>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetButtonDown("Equipment"))
-        {
-            equipmentUI.SetActive(!equipmentUI.activeSelf);
-            tooltip.text = "";
-        }
     }
 
     void UpdateUI(Equipment newItem, Equipment oldItem)
