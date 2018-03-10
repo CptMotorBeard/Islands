0027016aa8c9fbcdb62c45d0d126cf0c016734fa
Antonio <roccanator@gmail.com>
2018-03-10 13:56:44 -0500

    Base setup for instatiating items into the crafting item list, still a couple of bugs

---
+++ Assets/Scripts/CraftingUI.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class CraftingUI : MonoBehaviour {
+    public GameObject inventoryItems;
+    public GameObject craftingRecipes;
+
+    public GameObject inventoryItem;
+    public GameObject craftingRecipe;
+
+    Inventory inventory;
+
+    void Start()
+    {
+        inventory = Inventory.instance;
+        inventory.onItemChangedCallback += FillItems;
+    }
+
+    void FillItems()
+    {       
+
+        for (int i = 0; i < inventory.items.Length; i++)
+        {
+            InventoryItem currentItem = inventory.items[i];            
+
+            if (currentItem != null)
+            {
+                int quantity = currentItem.quantity;
+                for (int j = i; j < inventory.items.Length; j++)
+                {
+                    if (inventory.items[j] != null && inventory.items[j].item.id == currentItem.item.id)
+                    {
+                        quantity += inventory.items[j].quantity;
+                    }
+                }
+                GameObject o = Instantiate(inventoryItem);
+                o.transform.SetParent(inventoryItems.transform);
+            }
+        }
+    }
+}
