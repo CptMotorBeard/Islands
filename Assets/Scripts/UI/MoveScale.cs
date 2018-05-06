049a76f7f13c8977f2838703b7adb7b2223ffa76
Antonio <roccanator@gmail.com>
2018-05-06 11:41:22 -0400

    Pause menu now has a scale and location option for the toolbar

---
+++ Assets/Scripts/UI/MoveScale.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class MoveScale : MonoBehaviour {
+
+    Vector3 startLocation;
+
+    void Start()
+    {
+        startLocation = this.transform.position;
+    }
+
+    public void ChangeScale(float s)
+    {
+        this.transform.localScale = new Vector3(s, s, s);
+    }
+
+    public void ChangePosY(float y)
+    {
+        this.transform.position = startLocation + new Vector3(0, y);
+    }
+}
