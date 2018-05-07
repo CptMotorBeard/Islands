4b3554165d1a3fcc303eb3abb21563b1bcbd1968
Antonio <roccanator@gmail.com>
2018-05-07 15:43:06 -0400

    Added temporary player light so it's not too dark when testing

---
+++ Assets/Scripts/Lighting/PlayerLighting.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerLighting : MonoBehaviour {
+
+    public GameObject playerLight;
+	
+    // Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.F2))
+        {
+            playerLight.SetActive(!playerLight.activeSelf);
+        }
+	}
+}
