a7d857acb35f00f0489cc10b75fefbf2629f7d5f
Antonio <roccanator@gmail.com>
2018-05-13 13:43:01 -0400

    Set up a wolf + animations

---
+++ Assets/Scripts/Enemies/Character.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    Animator animator;
+    Rigidbody2D rb2d;
+    SpriteRenderer sprite;
+
+    bool facingLeft;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+
+        facingLeft = false;
+    }
+	
+	void FixedUpdate () {
+        sprite.flipX = facingLeft;
+	}
+}
