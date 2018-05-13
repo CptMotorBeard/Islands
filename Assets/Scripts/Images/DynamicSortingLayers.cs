dcb8746e25a96c073d5d623b203498dda2130367
Antonio <roccanator@gmail.com>
2018-05-13 11:36:04 -0400

    Dynamic sorting for layers and sprites

---
+++ Assets/Scripts/Images/DynamicSortingLayers.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicSortingLayers : MonoBehaviour {
+
+    public bool moveableObject = false;
+    public int offset = 0;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<SpriteRenderer>().sortingOrder = (Mathf.RoundToInt(transform.position.y * 100f) * -1) + offset;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (moveableObject)
+            GetComponent<SpriteRenderer>().sortingOrder = (Mathf.RoundToInt(transform.position.y * 100f) * -1) + offset;
+    }
+}
